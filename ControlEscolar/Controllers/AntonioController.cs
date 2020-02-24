3ba9134f05402ead98c4469ba96c76b18bfe76a7
antonio160698 <antonio.elizondolo@uanl.edu.mx>
2020-02-24 11:49:02 -0600

    [UPDATE] Se agrego mi controller

---
+++ ControlEscolar/Controllers/AntonioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlEscolar.Controllers
+{
+    public class AntonioController : Controller
+    {
+        // GET: Antonio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
