d1bc83f9b5a5b05ed7ccdf8f7651d3b86d3d43c2
Alejandro <alejandro_barrientos@hotmail.com>
2020-02-25 13:35:07 -0600

    [UPDATE]
    -Modelo de grupos

---
+++ ControlEscolar/Models/Entidades/Grupos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlEscolar.Models.Entidades
+{
+    public class Grupos
+    {
+        public int Materia { get; set; }
+        public int Profesor { get; set; }
+        public String Turno { get; set; }
+    }
+}
