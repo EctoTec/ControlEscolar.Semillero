245761e618ae07f8f65c0f965403ce08100dc3ed
antonio160698 <antonio.elizondolo@uanl.edu.mx>
2020-02-25 13:32:21 -0600

    cree una carpeta de lo que utilizare para materias y contiene areacontroller y carreracontroller

---
+++ ControlEscolar/Controllers/Materias/CarreraController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ControlEscolar.Controllers.Materias
+{
+    public class CarreraController : ApiController
+    {
+        // GET: api/Carrera
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Carrera/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Carrera
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Carrera/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Carrera/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
