8560b1709942eb42b8c8b7b0dcbdc5623cd7301c
antonio160698 <antonio.elizondolo@uanl.edu.mx>
2020-02-25 12:29:43 -0600

    Cree una carpeta de entidades con la entidad de materia y cree el controlador de api de materia

---
+++ ControlEscolar/Models/Entidades/Materia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlEscolar.Models.Entidades
+{
+    public class Materia
+    {
+        public String Nombre { get; set; }
+        public int Carrera { get; set; }
+        public int Area { get; set; }
+    }
+}
