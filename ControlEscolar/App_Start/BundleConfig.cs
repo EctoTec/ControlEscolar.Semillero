a9ab4d7d44456446f596d88a9a678f4b013e6f27
ijcharly <munoz.carlos19@gmail.com>
2020-02-26 12:57:14 -0600

    Agregue referencias del archivo .js

--- ControlEscolar/App_Start/BundleConfig.cs
+++ ControlEscolar/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace ControlEscolar
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/Funciones").Include(
+          "~/Scripts/Funciones.js"));
+
         }
     }
 }
