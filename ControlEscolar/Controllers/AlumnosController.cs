a7e746ea33cf1d9133e751479c16cf0f25d17458
Emmanuel Rdz <emmanuel_rdz16@hotmail.com>
2020-02-26 12:41:31 -0600

    [UPDATE] Se agrego entidad Alumnos

--- ControlEscolar/Controllers/AlumnosController.cs
+++ ControlEscolar/Controllers/AlumnosController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ControlEscolar.Models;
+using ControlEscolar.Models.Entidades;
 
 namespace ControlEscolar.Controllers
 {
@@ -14,5 +16,14 @@ namespace ControlEscolar.Controllers
             ViewBag.Title = "Alumnos";
             return View();
         }
+
+        /*public ActionResult Alumno()
+        {
+            List<Alumno> alumnos = new List<Alumno>();
+            using(CursoEscolarEntities con = new CursoEscolarEntities())
+            {
+
+            }
+        }*/
     }
 }
