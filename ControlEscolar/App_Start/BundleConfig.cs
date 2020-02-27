91b08f521b85a5669b403291ab3efd526fafcd53
dmunoz <danielamartz@icloud.com>
2020-02-27 10:11:47 -0600

    [UPDATE] add popper
    Agregué el popper en Bundle

--- ControlEscolar/App_Start/BundleConfig.cs
+++ ControlEscolar/App_Start/BundleConfig.cs
@@ -17,7 +17,8 @@ namespace ControlEscolar
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                       "~/Scripts/popper.js",
+                        "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
