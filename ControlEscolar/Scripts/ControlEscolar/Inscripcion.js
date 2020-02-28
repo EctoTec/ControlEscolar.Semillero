92ecf690c83045e8458f5c7624e21cc5412123ed
dmunoz <danielamartz@icloud.com>
2020-02-28 09:12:34 -0600

    [UPDATE] inscripcion controller de inscripcion, viewmodel, script, vistas

--- ControlEscolar/Scripts/ControlEscolar/Inscripcion.js
+++ ControlEscolar/Scripts/ControlEscolar/Inscripcion.js
@@ -1,23 +1,23 @@
-﻿document.querySelector('#btn-enviar').addEventListener('click', getDatos);
+﻿//document.querySelector('#btn-enviar').addEventListener('click', getDatos);
 
-function getDatos() {
-    const xhttp = new XMLHttpRequest();
+//function getDatos() {
+//    const xhttp = new XMLHttpRequest();
 
-    xhttp.open('GET', 'http://localhost:44387/api/Materia', true);
+//    xhttp.open('GET', 'http://localhost:44387/api/Materia', true);
 
-    xhttp.send();
+//    xhttp.send();
 
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 & this.status == 200) {
-            let datos = JSON.parse(this.responseText);
-            let list = document.querySelector('#list');
-            list.innerHTML = '';
+//    xhttp.onreadystatechange = function () {
+//        if (this.readyState == 4 & this.status == 200) {
+//            let datos = JSON.parse(this.responseText);
+//            let list = document.querySelector('#list');
+//            list.innerHTML = '';
 
-            for (let item of datos) {
-                list.innerHTML += `
-                    <button type="button" class="list-group-item list-group-item-action">${item.Nombre}</button>
-                `
-            }
-        }
-    }
-}
+//            for (let item of datos) {
+//                list.innerHTML += `
+//                    <button type="button" class="list-group-item list-group-item-action">${item.Nombre}</button>
+//                `
+//            }
+//        }
+//    }
+//}
