be921d1f112caf9bbadf8ecbc3eabfbfc410994a
Alejandro <alejandro_barrientos@hotmail.com>
2020-02-28 11:08:12 -0600

    Cambio de grupo

--- ControlEscolar/Controllers/GruposController.cs
+++ ControlEscolar/Controllers/GruposController.cs
@@ -10,25 +10,5 @@ namespace ControlEscolar.Controllers.Grupos
     public class GruposController : ApiController
     {
         //POST: api/Grupo
-        /*
-        public bool Post(d_Grupos grupos)
-        {
-            Boolean guardar = false;
-            using(CursoEscolarEntities contexto = new CursoEscolarEntities())
-            {
-                AltaGrupo grup = new AltaGrupo
-                {
-                    Grp_Materia_Id = grupos.Materia,
-                    Grp_Profesor_Id = grupos.Profesor,
-                    Grp_Turno = grupos.Turno
-                };
-                contexto.Grupo.Add(grup);
-                contexto.SaveChanges();
-                guardar = true;
-            }
-            return guardar;
-        }
-        */
-
     }
 }
