c80ab9f89f8e27dcf7789bc579af00271d5f33ef
Alejandro <alejandro_barrientos@hotmail.com>
2020-02-28 09:57:57 -0600

    Algunos Cambios

--- ControlEscolar/Models/Grupo.cs
+++ ControlEscolar/Models/Grupo.cs
@@ -12,10 +12,10 @@ namespace ControlEscolar.Models
     using System;
     using System.Collections.Generic;
     
-    public partial class Grupo
+    public partial class AltaGrupo
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Grupo()
+        public AltaGrupo()
         {
             this.Inscripcion = new HashSet<Inscripcion>();
         }
