aea27b64bbbff32f0871183873eebcc3462a8361
Alejandro <alejandro_barrientos@hotmail.com>
2020-02-28 10:27:03 -0600

    CAMBIO OBLIGATORIO

--- ControlEscolar/Controllers/GruposController.cs
+++ ControlEscolar/Controllers/GruposController.cs
@@ -10,24 +10,25 @@ namespace ControlEscolar.Controllers.Grupos
     public class GruposController : ApiController
     {
         //POST: api/Grupo
-
+        /*
         public bool Post(d_Grupos grupos)
         {
             Boolean guardar = false;
             using(CursoEscolarEntities contexto = new CursoEscolarEntities())
             {
-                AltaGrupo grupo1 = new AltaGrupo
+                AltaGrupo grup = new AltaGrupo
                 {
                     Grp_Materia_Id = grupos.Materia,
                     Grp_Profesor_Id = grupos.Profesor,
                     Grp_Turno = grupos.Turno
                 };
-                contexto.Grupo.Add(grupo1);
+                contexto.Grupo.Add(grup);
                 contexto.SaveChanges();
                 guardar = true;
             }
             return guardar;
         }
+        */
 
     }
 }
--- ControlEscolar/Scripts/ControlEscolar/Grupos.js
+++ ControlEscolar/Scripts/ControlEscolar/Grupos.js
@@ -1,6 +1,34 @@
-﻿let btn_Add_Carrera = document.getElementById("Ag_Add_Carrera");
-let btn_Add_Area = document.getElementById("Ag_Add_Area");
+﻿let btn_Add_Grupo = document.getElementById("save");
 
-let input_C_Nombre = document.getElementById("C_Nombre");
-let input_C_Nivel = document.getElementById("C_Nivel");
-let input_A_Nombre = document.getElementById("A_Nombre");
+var input_Materia = document.getElementById("Materia");
+var input_Maestro = document.getElementById("Maestro");
+var input_Turno = document.getElementById("Turno");
+
+input_Materia.addEventListener('change', function () {
+    var selectedOption1 = this.options[select.selectedIndex];
+    console.log(selectedOption1.value + ":" + selectedOption1.text);
+});
+input_Maestro.addEventListener('change', function () {
+    var selectedOption2 = this.options[select.selectedIndex];
+    console.log(selectedOption2.value + ":" + selectedOption2.text);
+});
+input_Turno.addEventListener('change', function () {
+    var selectedOption3 = this.options[select.selectedIndex];
+    console.log(selectedOption3.value + ":" + selectedOption.text);
+});
+
+btn_Add_Grupo.onclick = () => {
+    $.ajax({
+        type: "POST",
+        url: "/api/Grupo",
+        data: {
+            "Materia": input_Materia.value,
+            "Profesor": input_Mestro.value,
+            "Turno": input_Turno.value
+        },
+        dataType: "JSON",
+        success: (response) => {
+            $('#')
+        }
+    })
+}
