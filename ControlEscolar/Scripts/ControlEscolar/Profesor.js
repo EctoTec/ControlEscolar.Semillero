6bf9cca98d821080b48bef6e6046a33252d4692a
Héctor Soria <totis_raya2@hotmail.com>
2020-03-04 12:53:17 -0600

    Cambios NumeroProfesor

--- ControlEscolar/Scripts/ControlEscolar/Profesor.js
+++ ControlEscolar/Scripts/ControlEscolar/Profesor.js
@@ -73,24 +73,21 @@ let drawTableProfesor = (arreglo) => {
 
 function getNumeroProfesor(id) {
     if (id < 1) {
-        id = "000000" + id;
-    }
-    else if (id < 10) {
         id = "00000" + id;
     }
-    else if (id < 100) {
+    else if (id < 10) {
         id = "0000" + id;
     }
-    else if (id < 1000) {
+    else if (id < 100) {
         id = "000" + id;
     }
-    else if (id < 10000) {
+    else if (id < 1000) {
         id = "00" + id;
     }
-    else if (id < 100000) {
+    else if (id < 10000) {
         id = "0" + id;
     }
-    else if (id >= 100000) {
+    else if (id >= 10000) {
         id = id;
     }
     return id;
