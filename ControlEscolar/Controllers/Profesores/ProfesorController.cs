8c410971dde76de10faa350c6fb9b2d469a55e41
ricar <ricar@ROKY-7U7>
2020-03-06 09:37:27 -0600

    Editar en profesores y agregacion de varios modales

--- ControlEscolar/Controllers/Profesores/ProfesorController.cs
+++ ControlEscolar/Controllers/Profesores/ProfesorController.cs
@@ -38,6 +38,22 @@ namespace ControlEscolar.Controllers.Profesores
             return eliminar;
         }
 
+        public bool Put(c_Profesores prfs ,int id)
+        {
+            using(CursoEscolarEntities contexto = new CursoEscolarEntities())
+            {
+                Profesor editProfesor = (from Prof in contexto.Profesor
+                                         where Prof.Prf_Id == id
+                                         select Prof).FirstOrDefault();
+                editProfesor.Prf_Nombre = prfs.Nombre;
+                editProfesor.Prf_Apellido = prfs.Apellido;
+                editProfesor.Prf_Area_Id = prfs.Area;
+                contexto.SaveChanges();
+
+            }
+            return true;
+        }
+
 
         public tab_Profesores[] GET()
         {
@@ -51,7 +67,8 @@ namespace ControlEscolar.Controllers.Profesores
                                 Id = p.Prf_Id,
                                 Nombre = p.Prf_Nombre,
                                 Apellido = p.Prf_Apellido,
-                                Area = a.Area_Nombre
+                                Area = a.Area_Nombre,
+                                Area_Id = a.Area_Id
                             };
                 LProf = query.ToList();
             }
